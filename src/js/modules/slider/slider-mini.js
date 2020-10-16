7e01d621f605a62e61468b8513c597db3ceb97ad
Alex <cigan@ua.fm>
2020-10-16 16:43:24 +0300

    lesson 5, slider, addEventListener 'click' for all btns of mini sliders and add SetInterval for third mini slider

--- src/js/modules/slider/slider-mini.js
+++ src/js/modules/slider/slider-mini.js
@@ -1,9 +1,60 @@
 import Slider from './slider';
 
 export default class MiniSlider extends Slider {
-    constructor(container, next, prev) {
-        super(container, next, prev);
+    constructor(container, next, prev, activeClass, animate, autoplay) {
+        super(container, next, prev, activeClass, animate, autoplay);
     }
+
+    decorizeSlides() {
+        this.slides.forEach(slide => {
+            slide.classList.remove(this.activeClass);
+            if (this.animate) {
+                slide.querySelector('.card__title').style.opacity = '0.4';
+                slide.querySelector('.card__controls-arrow').style.opacity = '0';
+            }
+        });
+        if (!this.slides[0].closest('button')) {     // closest - возвращает ближайший или самого себя
+            this.slides[0].classList.add(this.activeClass);
+        }
+        if (this.animate) {
+            this.slides[0].querySelector('.card__title').style.opacity = '1';
+            this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';
+        }
+    }
+
+    nextSlide() {
+        if (this.slides[1].tagName == "BUTTON" && this.slides[2].tagName == "BUTTON") {
+            this.container.appendChild(this.slides[0]);     // slide
+            this.container.appendChild(this.slides[1]);     // button
+            this.container.appendChild(this.slides[2]);     // button #2
+            this.decorizeSlides();
+
+        } else if (this.slides[1].tagName == "BUTTON") {
+            this.container.appendChild(this.slides[0]);     // slide
+            this.container.appendChild(this.slides[1]);     // button
+            this.decorizeSlides();
+        } else {
+            this.container.appendChild(this.slides[0]);
+            this.decorizeSlides();
+        }
+    }
+
+    bindTriggers() {
+        this.next.addEventListener('click', () => this.nextSlide());
+
+        this.prev.addEventListener('click', () => {
+
+            for (let i = this.slides.length - 1; i > 0; i--) {         // запускаем цикл с конца псевдомассива
+                if (this.slides[i].tagName !== "BUTTON") {
+                    let active = this.slides[i];
+                    this.container.insertBefore(active, this.slides[0]);
+                    this.decorizeSlides();
+                    break;
+                }
+            }
+        });
+    }
+
     init() {
         this.container.style.cssText = `
         display: flex;
@@ -11,5 +62,12 @@ export default class MiniSlider extends Slider {
         overflow: hidden;
         align-items: flex-start;
         `;
+
+        this.bindTriggers();
+        this.decorizeSlides();
+
+        if (this.autoplay) {
+            setInterval(() => this.nextSlide(), 5000);
+        }
     }
 }
--- src/js/modules/slider/slider.js
+++ src/js/modules/slider/slider.js
@@ -1,11 +1,21 @@
 export default class Slider {       // можно сразу експортировать, чтобы не писать в конце. Наименование Class-ов пишется с большой буквы
     // constructor(page, btns) {       // page / container - одна из страниц, сайт многостраничный; bnts - кнопки переключения элементов
-    constructor({ container = null, btns = null, next = null, prev = null } = {}) {       // передаем "", null или {} (дефолтные значения), что-бы небыло ошибки если не будет аргумента, container - одна из страниц, сайт многостраничный; bnts - кнопки переключения элементов
+    constructor({ container = null,
+        btns = null,
+        next = null,
+        prev = null,
+        activeClass = "",
+        animate,
+        autoplay
+    } = {}) {       // передаем "", null или {} (дефолтные значения), что-бы небыло ошибки если не будет аргумента, container - одна из страниц, сайт многостраничный; bnts - кнопки переключения элементов
         this.container = document.querySelector(container); // page / moduleapp - также и главный блок на странице (верстке)
         this.slides = this.container.children; //  слайды (отдельные div) внутри page / moduleapp
         this.btns = document.querySelectorAll(btns);
         this.prev = document.querySelector(prev);       // кнопки для мини слайдеров
         this.next = document.querySelector(next);       // кнопки для мини слайдеров
+        this.activeClass = activeClass;                 // класс активности слайда
+        this.animate = animate;                         // анимация активного слайда
+        this.autoplay = autoplay;
         this.slideIndex = 1;        // определяет индекс слайда
     }
 }
