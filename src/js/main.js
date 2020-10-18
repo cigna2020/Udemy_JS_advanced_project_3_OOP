86efca1a482439ad965455351f91627571bbc52c
Alex <cigan@ua.fm>
2020-10-18 16:22:24 +0300

    lesson 11, show info, show or hide it after clicking on a button (plus)

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Forms from './modules/forms';
+import ShowInfo from './modules/showInfo';
 
 window.addEventListener('DOMContentLoaded', () => {
     // const slider = new Slider('.page', '.next');     // slider - экземпляр класса
@@ -44,4 +45,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();   // альтернативный метод вызова, без создания переменной
     new Forms('.form').init();
+
+    new ShowInfo('.plus__content').init();
 });
